4af9862da0a516b08af8e5e546075cd058ce1f6f
jordan h <jordanhiatt@u.boisestate.edu>
2021-03-11 17:38:10 -0700

    Update DeckOfCards.sol

--- DeckOfCards.sol
+++ DeckOfCards.sol
@@ -1,40 +1,69 @@
 pragma solidity 0.5.1;
-
-contract DeckOfCards {
+pragma experimental ABIEncoderV2;
+import "./DeckOfCards.sol";
+contract PlayerHand {
 
     string value;
-    string public card;
+    // string public card;
     string[] private ranks;
-    string[] public deck;
+    Card[] public hand;
+    Card[] public removedCards;
     string[] suits;
-
+    Card public playerCard;
     
-    // function cancat(string memory a, string memory b) public view returns(string memory){
-    //     return(string(abi.encodePacked(a,"/",b)));
-    // }
-    // function firstCard(DeckOfCards d) public view returns(string memory) {
-    //     return d.deck[0];
-    // }
     
-    constructor() public {
-        buildDeck();
+    struct Card {
+        uint _id;
+        string _rank;
+        string _suit;
+        // string _toString;
     }
     
-    function buildDeck() public {
-        suits = ['H', 'C', 'D', 'S'];
-        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
-        //build deck
-        uint256 i;
-        uint256 j;
-        for(i = 0; i < suits.length; i++) {
-            for(j = 0; j < ranks.length; j++) {
-                // deck.push(cancat(ranks[j], suits[i]));
-                // having trouble concatenating strings or returning them
-                deck.push('test');
+    function add(Card memory c) public {
+        hand.push(c);
+    }
+    
+    // this should return an array of cards 
+    function removeCardsOfRank(string memory s) public returns (Card[] memory){
+        delete removedCards;
+        uint foundCards = 0;
+        for(uint j = 0; j < hand.length; j++) {
+            // Card memory c = hand[j];
+            // Compare bytes of strings
+            if(keccak256(bytes(hand[j]._rank)) == keccak256(bytes(s))) {
+            // if(c._rank == s) {
+                foundCards += 1;
+                // at the end we will pop 'foundCards' many cards into a cardArray for placing into other players hand
+                Card memory temp = hand[hand.length-foundCards];
+                hand[hand.length-foundCards] = hand[j];
+                hand[j] = temp;
             }
         }
+        //if there are 2 found cards, should only pop the last 2 elements
+        while(foundCards >= 0) {
+            // Card memory popped = hand[hand.length];
+            removedCards.push(hand[hand.length-1]);
+            hand.pop();
+            foundCards -= 1;
+        }
+        // whatever uses this must check if removedCards is empty. 
+        return removedCards;
     }
-    function clearDeck() public {
-        delete deck;
+    
+    function checkIfEmpty() public view returns (bool) {
+        if(hand.length == 0) {
+            return true;
+        } else  {
+            return false;
+        }
+    }
+    
+    
+    // pullOutAndAddToHand()
+    // hand.add(deck.pull())
+    
+    
+    function clearHand() public {
+        delete hand;
     }
 }
