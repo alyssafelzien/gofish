cf4cbcd7c69073a9a29332278d882be069670c4c
Alyssa Felzien <40674345+alyssafelzien@users.noreply.github.com>
2021-03-12 22:52:36 -0700

    Update DeckOfCards.sol

--- DeckOfCards.sol
+++ DeckOfCards.sol
@@ -4,17 +4,14 @@ pragma experimental ABIEncoderV2;
 contract DeckOfCards {
     
     struct Card {
-        uint _id;
-        string _rank;
-        string _suit;
-        // string _toString;
+        uint8 id;
+        uint8 rank;
+        uint8 suit;
     }
-    
-    string value;
-    // string public card;
-    string[] private ranks;
+
+    uint8[] private ranks;
     Card[] public deck;
-    string[] suits;
+    uint8[] private suits;
     Card public card;
     uint256 public deckLength;
     
@@ -23,12 +20,12 @@ contract DeckOfCards {
         return uint8(uint256(block.timestamp)%(deck.length));
     }
 
-    function addCard(uint _id, string memory _rank, string memory _suit) public {
+    function addCard(uint8 _id, uint8 _rank, uint8  _suit) public {
         card = Card(_id, _rank, _suit);
         deck.push(card);
     }
     
-    function checkIfEmpty() public view returns (bool) {
+    function isEmpty() public view returns (bool) {
         if(deck.length == 0) {
             return true;
         } else  {
@@ -39,11 +36,12 @@ contract DeckOfCards {
     //you'd do something like hand.addCard(deck.pullCard())
     function pullCard() public returns (Card memory) {
         if(deck.length != 0) {
-            deck.pop();
-            // return deck[deck.length-1];
-            //update deckLength to see how many are left
-            deckLength = deck.length;
-            return deck[random()];
+             uint8 index = random();
+             Card memory retCard = deck[index];
+             deck[index] = deck[deck.length - 1];
+             deck.pop();
+             deckLength = deck.length;
+             return retCard;
         } 
     }
     
@@ -51,18 +49,12 @@ contract DeckOfCards {
         buildDeck();
     }
     
-    
-    // function firstCard() public returns(string memory) {
-    //     string memory x = deck[deck.length];
-    //     delete deck[deck.length];
-    //     return x;
-    // }
-    function buildDeck() public {
-        suits = ['H', 'C', 'D', 'S'];
-        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+   function buildDeck() public {
+        suits = [0, 1, 2, 3]; //['H', 'C', 'D', 'S'];
+        ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]; //['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
         //build deck
-        uint256 i;
-        uint256 j;
+        uint8 i;
+        uint8 j;
         for(i = 0; i < suits.length; i++) {
             for(j = 0; j < ranks.length; j++) {
                 
@@ -71,10 +63,69 @@ contract DeckOfCards {
         }
     }
     
+    
     //pullOutAndAddToHand()
     
     
     function clearDeck() public {
         delete deck;
+    }
+    
+        
+    function getStringSuit(uint8 suit) public pure returns(string memory) {
+        if(suit == 0) {
+            return 'H';
+        }
+        if(suit == 1) {
+            return 'C';
+        }
+        if(suit == 2) {
+            return 'D';
+        }
+        if(suit == 3) {
+            return 'S';
+        }
+    }
+    
+    function getStringRank(uint8 rank) public pure returns(string memory) {
+        if(rank == 1) {
+            return 'Ace';
+        }
+        if(rank == 2) {
+            return '2';
+        }
+        if(rank == 3) {
+            return '3';
+        }
+        if(rank == 4) {
+            return '4';
+        }
+        if(rank == 5) {
+            return '5';
+        }
+        if(rank == 6) {
+            return '6';
+        }
+        if(rank == 7) {
+            return '7';
+        }
+        if(rank == 8) {
+            return '8';
+        }
+        if(rank == 9) {
+            return '9';
+        }
+        if(rank == 10) {
+            return '10';
+        }
+        if(rank == 11) {
+            return 'Jack';
+        }
+        if(rank == 12) {
+            return 'Queen';
+        }
+        if(rank == 13) {
+            return 'King';
+        }
     }
 }
